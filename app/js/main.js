d63ae0a360944e3c872f66250dbaaa366f515c60
QraisinQ <hs1985irin@gmail.com>
2021-05-31 03:14:43 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,25 @@ $(function () {
     slidesToShow: 4,
     slidesToScroll: 4,
   });
+  $(".js-range-slider").ionRangeSlider({
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    type: "double",
+    prefix: "$",
+  });
+  $(".icon-th-list").on("click", function () {
+    $(".product__item").addClass("list");
+    $(".icon-th-list").toggleClass("active");
+    $(".icon-th-large").toggleClass("active");
+  });
+
+  $(".icon-th-large").on("click", function () {
+    $(".product__item").removeClass("list");
+    $(".icon-th-large").addClass("active");
+    $(".icon-th-list").removeClass("active");
+  });
 
   var mixer = mixitup(".products__inner-box");
 });
